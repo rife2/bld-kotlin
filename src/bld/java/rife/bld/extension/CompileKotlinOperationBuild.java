cdf6294b39454ecdba4d8a48f7c1f22d6109191b
Erik C. Thauvin <erik@thauvin.net>
2024-05-07 11:32:54 -0700

    Version 0.9.4

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -33,7 +33,7 @@ public class CompileKotlinOperationBuild extends Project {
     public CompileKotlinOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-kotlin";
-        version = version(0, 9, 3);
+        version = version(0, 9, 4);
 
         javaRelease = 17;
         downloadSources = true;
@@ -52,7 +52,7 @@ public class CompileKotlinOperationBuild extends Project {
                 .include(dependency("org.jetbrains.dokka", "javadoc-plugin", dokka))
                 .include(dependency("org.jetbrains.dokka", "gfm-plugin", dokka))
                 .include(dependency("org.jetbrains.dokka", "jekyll-plugin", dokka))
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 1)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 2)))
@@ -81,7 +81,7 @@ public class CompileKotlinOperationBuild extends Project {
                 .license(
                         new PublishLicense()
                                 .name("The Apache License, Version 2.0")
-                                .url("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.txt")
                 )
                 .scm(
                         new PublishScm()
