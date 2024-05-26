6f5d20b2c061dffaa88b8b92a48e9e5b2f1ce167
Erik C. Thauvin <erik@thauvin.net>
2024-05-26 12:34:31 -0700

    Added diff of sourceSet help

--- checkcliargs.sh
+++ checkcliargs.sh
@@ -3,12 +3,15 @@
 TMPNEW=/tmp/checkcliargs-new
 TMPOLD=/tmp/checkcliargs-old
 
-cd lib/compile
-java -cp "*" -jar dokka-cli-*.jar -h >$TMPNEW
-
-cd ../../examples/lib/bld
-java -cp "*" -jar dokka-cli-*.jar -h >$TMPOLD
+java -cp "lib/compile/*" -jar lib/compile/dokka-cli-*.jar -h >$TMPNEW
+java -cp "/examples/lib/bld*" -jar examples/lib/bld/dokka-cli-*.jar -h >$TMPOLD
 
 diff $TMPOLD $TMPNEW
+
+java -cp "lib/compile/*" -jar lib/compile/dokka-cli-*.jar -sourceSet -h >$TMPNEW
+java -cp "/examples/lib/bld*" -jar examples/lib/bld/dokka-cli-*.jar -sourceSet -h >$TMPOLD
+
+diff $TMPOLD $TMPNEW
+
 
 rm -rf $TMPNEW $TMPOLD
