27bc3a0997a64d9b844212dfb3dd489ac7b5105e
Erik C. Thauvin <erik@thauvin.net>
2024-08-30 15:54:36 -0700

    Version 1.0.1

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -33,7 +33,7 @@ public class CompileKotlinOperationBuild extends Project {
     public CompileKotlinOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-kotlin";
-        version = version(1, 0, 1, "SNAPSHOT");
+        version = version(1, 0, 1);
 
         javaRelease = 17;
 
