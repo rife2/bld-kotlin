ecfa80b66e8eabd0785cc4a9f6df18978143d7d7
Erik C. Thauvin <erik@thauvin.net>
2025-08-15 23:58:15 -0700

    Bump Kotlin to version 2.2.10

--- scripts/checkcliargs.sh
+++ scripts/checkcliargs.sh
@@ -4,7 +4,7 @@ new=/tmp/checkcliargs-new
 old=/tmp/checkcliargs-old
 
 kotlinc -h 2>$new
-~/.sdkman/candidates/kotlin/2.1.21/bin/kotlinc -h 2>$old
+~/.sdkman/candidates/kotlin/2.2.0/bin/kotlinc -h 2>$old
 
 code --diff --wait $old $new
 
