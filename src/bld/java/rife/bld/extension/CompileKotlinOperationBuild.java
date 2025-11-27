b3550850653569a7b271e7fb82034889d8e29ec0
Erik C. Thauvin <erik@thauvin.net>
2025-11-27 11:20:43 -0800

    Bump AssertJ to version 3.27.6

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -56,7 +56,7 @@ public class CompileKotlinOperationBuild extends Project {
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone",
                         version(1, 13, 4)))
                 .include(dependency("org.assertj", "assertj-core",
-                        version(3, 27, 4)));
+                        version(3, 27, 6)));
 
         javadocOperation()
                 .javadocOptions()
