6a0a86e8e3b6bb01114bb9489bb1e2bf4c8a26e4
Erik C. Thauvin <erik@thauvin.net>
2026-01-20 14:16:49 -0800

    Bump JUnit to version 6.0.2

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -42,7 +42,7 @@ public class CompileKotlinOperationBuild extends Project {
 
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_SNAPSHOTS, RIFE2_RELEASES);
 
-        var junit = version(6, 0, 1);
+        var junit = version(6, 0, 2);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
                         version(0, 9, 0, "SNAPSHOT")))
