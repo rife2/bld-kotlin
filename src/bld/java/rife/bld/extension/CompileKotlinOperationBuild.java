dc41af2019f9e9fc5bf18b67df5b69bf4093070a
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 23:49:57 -0700

    Bump bld to version 3.0.0

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -48,8 +48,7 @@ public class CompileKotlinOperationBuild extends Project {
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
                         version(1, 3, 0)))
-                .include(dependency("com.uwyn.rife2", "bld",
-                        version(2, 4, 0, "SNAPSHOT")));
+                .include(dependency("com.uwyn.rife2", "bld", version(3, 0, 0)));
         scope(provided)
                 .include(dependency("org.jspecify", "jspecify", "1.0.1"))
                 .include(dependency("com.github.spotbugs", "spotbugs-annotations",
