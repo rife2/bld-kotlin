2c3a1ebd3cd5f962be0e5e0c35ee3a0efe380a54
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 20:16:30 -0700

    Bump Extensions Tools to version 1.3.0

--- src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
+++ src/bld/java/rife/bld/extension/CompileKotlinOperationBuild.java
@@ -47,7 +47,7 @@ public class CompileKotlinOperationBuild extends Project {
         var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
-                        version(1, 3, 0, "SNAPSHOT")))
+                        version(1, 3, 0)))
                 .include(dependency("com.uwyn.rife2", "bld",
                         version(2, 4, 0, "SNAPSHOT")));
         scope(provided)
